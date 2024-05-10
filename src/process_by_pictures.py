cc75a83778ad9d387a6147a6802fc012f114d986
vinog <vinogradskiy.n@gmail.com>
2024-05-10 16:48:10 +0200

    CHECKPOINT - use compreface for face detection and verification

--- src/process_by_pictures.py
+++ src/process_by_pictures.py
@@ -1,10 +1,31 @@
 import os
 import cv2
-import face_recognition
-import pandas as pd
-from collections import defaultdict
-from utils import extract_subtitles_from_srt, subtitle_to_ms, Subtitle, FACES_DIR, CATEGORIZED_DIR
 import shutil
+from compreface import CompreFace
+from compreface.service import RecognitionService, DetectionService, VerificationService
+from compreface.collections import FaceCollection
+from compreface.collections.face_collections import Subjects
+from utils import extract_subtitles_from_srt, subtitle_to_ms, FACES_DIR, CATEGORIZED_DIR, ORIGINAL_VIDEO, SUBTITLES, FRAMES_DIR
+
+DOMAIN: str = 'http://localhost'
+PORT: str = '8000'
+API_KEY_RECOGNITION: str = '964aa302-aca5-4712-8cf3-1c6929b59b91'
+API_KEY_DETECTION: str = 'eae4480b-cf40-4b28-ab4c-ddc5c349e8ba'
+API_KEY_VERIFICATION: str = 'd75fe71e-8fad-4767-8fda-974349a4d4f0'
+
+compre_face: CompreFace = CompreFace(DOMAIN, PORT, options={
+    "limit": 0,
+    "det_prob_threshold": 0.8,
+    "prediction_count": 1,
+    "face_plugins": "calculator,age,gender",
+    "status": "true"
+})
+
+recognition: RecognitionService = compre_face.init_face_recognition(API_KEY_RECOGNITION)
+face_collection: FaceCollection = recognition.get_face_collection()
+subjects: Subjects = recognition.get_subjects()
+detection: DetectionService = compre_face.init_face_detection(API_KEY_DETECTION)
+verification: VerificationService = compre_face.init_face_verification(API_KEY_VERIFICATION)
 
 # Remove and recreate FACES_DIR
 if os.path.exists(FACES_DIR):
@@ -16,75 +37,73 @@ if os.path.exists(CATEGORIZED_DIR):
     shutil.rmtree(CATEGORIZED_DIR)
 os.makedirs(CATEGORIZED_DIR)
 
-curr_dir = os.path.dirname(os.path.abspath(__file__))
-source_dir = os.path.join(curr_dir, "source")
-tmp_dir = os.path.join(curr_dir, "tmp")
-ORIGINAL_VIDEO = os.path.join(source_dir, 'original_video_full.mp4')
-SUBTITLES = os.path.join(source_dir, 'subtitles.srt')
-CSV = os.path.join(tmp_dir, 'final.csv')
+# # Remove and recreate CATEGORIZED_DIR
+# if os.path.exists(FRAMES_DIR):
+#     shutil.rmtree(FRAMES_DIR)
+# os.makedirs(FRAMES_DIR)
+
+def detect_single_face():
+    frame_dir = os.listdir(FRAMES_DIR)
+    for frame in frame_dir:
+        frame_path = os.path.join(FRAMES_DIR, frame)
+        output_path = os.path.join(FACES_DIR, frame)
+        result = detection.detect(frame_path)
+        
+        if result.get('code') and result.get('code') == 28:
+            continue
+        
+        if len(result.get('result')) == 1:
+            face_info = result.get('result')[0]
+            box = face_info['box']
+
+            # Load the image using OpenCV
+            image = cv2.imread(frame_path)
+
+            # Crop the face region from the image
+            x1, y1 = box['x_min'], box['y_min']
+            x2, y2 = box['x_max'], box['y_max']
+            face_image = image[y1:y2, x1:x2]
+
+            # Save the face image using OpenCV
+            cv2.imwrite(output_path, face_image)
 
 def extract_frames_from_video(subtitles):
     video = cv2.VideoCapture(ORIGINAL_VIDEO)
-    
     for subtitle in subtitles:
         sub_start = subtitle.start
         sub_start_ms = subtitle_to_ms(sub_start)
         video.set(cv2.CAP_PROP_POS_MSEC, sub_start_ms)
         success, image = video.read()
-        cv2.imwrite(os.path.join(FACES_DIR, f"{sub_start_ms}.jpg"), image)
+        img_path = os.path.join(FRAMES_DIR, f"{sub_start_ms}.jpg")
+        cv2.imwrite(img_path, image)
 
-def detect_faces_in_images():
-    face_encodings = {}
-    face_names = defaultdict(list)
+def categorize_faces():
+    faces_dir = os.listdir(FACES_DIR)
+    for face in faces_dir[:]:  # Create a copy of the list
+        face_path = os.path.join(FACES_DIR, face)
+        matched_face_dir = None
+        max_similarity = 0
 
-    for image_path in os.listdir(FACES_DIR):
-        image = face_recognition.load_image_file(os.path.join(FACES_DIR, image_path))
-        face_locations = face_recognition.face_locations(image)
+        for existing_face_dir in os.listdir(CATEGORIZED_DIR):
+            existing_face_path = os.path.join(CATEGORIZED_DIR, existing_face_dir, os.listdir(os.path.join(CATEGORIZED_DIR, existing_face_dir))[0])
+            result = verification.verify(face_path, existing_face_path)
+            similarity = result['result'][0]['face_matches'][0]['similarity']
 
-        if len(face_locations) == 1:
-            face_encoding = face_recognition.face_encodings(image, face_locations)[0]
-            name = image_path.split(".")[0]
+            if similarity > max_similarity:
+                max_similarity = similarity
+                matched_face_dir = existing_face_dir
 
-            if not any(face_recognition.compare_faces(list(face_encodings.values()), face_encoding)):
-                face_encodings[name] = face_encoding
-            else:
-                for key, value in face_encodings.items():
-                    if face_recognition.compare_faces([value], face_encoding)[0]:
-                        face_names[key].append(os.path.join(FACES_DIR, image_path))
-
-    return face_names
-
-def categorize_faces(face_names):
-    for actor_name, image_paths in face_names.items():
-        actor_dir = os.path.join(CATEGORIZED_DIR, actor_name)
-        os.makedirs(actor_dir, exist_ok=True)
-        for image_path in image_paths:
-            if os.path.exists(image_path):
-                os.rename(image_path, os.path.join(actor_dir, os.path.basename(image_path)))
-
-def save_to_csv(face_names, subtitles):
-    data = []
-    for subtitle in subtitles:
-        subtitle_start_ms = subtitle_to_ms(subtitle.start)
-        for actor_name, image_paths in face_names.items():
-            for image_path in image_paths:
-                if str(subtitle_start_ms) in image_path:
-                    data.append({
-                        'subtitle.start': subtitle.start,
-                        'subtitle_to_ms(subtitle.start)': subtitle_start_ms,
-                        'actor_name': actor_name,
-                        'actor_images_path': image_path
-                    })
-
-    df = pd.DataFrame(data)
-    df.to_csv(CSV, index=False)
-
-def main():
-    subtitles = extract_subtitles_from_srt(SUBTITLES)
-    extract_frames_from_video(subtitles)
-    face_names = detect_faces_in_images()
-    categorize_faces(face_names)
-    save_to_csv(face_names, subtitles)
+        if max_similarity > 0.7:
+            shutil.move(face_path, os.path.join(CATEGORIZED_DIR, matched_face_dir))
+            faces_dir.remove(face)  # Remove the moved file from the list
+        else:
+            new_dir = os.path.join(CATEGORIZED_DIR, face.split('.')[0])
+            os.makedirs(new_dir, exist_ok=True)
+            shutil.move(face_path, new_dir)
+            faces_dir.remove(face)  # Remove the moved file from the list
 
 if __name__ == "__main__":
-    main()
+    # subtitles = extract_subtitles_from_srt(SUBTITLES)
+    # extract_frames_from_video(subtitles)
+    detect_single_face()
+    categorize_faces()
