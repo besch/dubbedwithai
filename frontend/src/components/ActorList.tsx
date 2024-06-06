3d952e28cccbf2456b91ad98038bd60e8a7b4a4a
vinog <vinogradskiy.n@gmail.com>
2024-06-06 13:34:32 +0200

    actorList impr

--- frontend/src/components/ActorList.tsx
+++ frontend/src/components/ActorList.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useRef, useEffect } from "react";
+import { Suspense, lazy, memo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/store/store";
 import {
@@ -10,27 +10,90 @@ import {
 } from "@/components/ui/card";
 import {
   Select,
-  SelectContent,
   SelectItem,
+  SelectContent,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { FixedSizeList } from "react-window";
+
+const LazyLoadedImage = lazy(() => import("next/image"));
+
+const LazyImage = ({ src, width, height, ...props }) => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <LazyLoadedImage
+      src={src}
+      width={width}
+      height={height}
+      {...props}
+      alt=""
+    />
+  </Suspense>
+);
+
+const MemoizedSelectItem = memo(({ value, className }) => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <SelectItem value={value} className={className}>
+      <LazyImage
+        className="h-60px w-60px p-2"
+        src={`data:image/png;base64,${value}`}
+        width={60}
+        height={60}
+      />
+    </SelectItem>
+  </Suspense>
+));
+
+const useVirtualizedSelectItems = (faceData) => {
+  const itemData = Object.entries(faceData.encoded_images);
+  const itemCount = itemData.length;
+
+  const getItemKey = (index) => itemData[index][0];
+
+  const renderItem = ({ index, style }) => {
+    const [key, image] = itemData[index];
+    return (
+      <div style={style}>
+        <MemoizedSelectItem key={key} value={image} className="capitalize" />
+      </div>
+    );
+  };
+
+  return { itemCount, getItemKey, renderItem };
+};
 
 export default function ActorList() {
   const dispatch = useDispatch();
   const faceData = useSelector((state: RootState) => state.subtitle.faceData);
+  const { itemCount, getItemKey, renderItem } =
+    useVirtualizedSelectItems(faceData);
 
   return (
-    <div className="m-5 w-1/3 flex flex-col h-[500px] overflow-y-auto">
+    <div className="m-5 w-1/2 flex flex-col h-[500px] overflow-y-auto">
       {Object.entries(faceData.encoded_images).map(([key, image], index) => (
         <div className="flex flex-row items-center" key={index}>
           <img
             className="h-60px w-60px p-2"
             key={index}
             src={`data:image/png;base64,${image}`}
-            alt={`Encoded Image ${index}`}
           />
-          <span className="ml-10 text-white">{key}</span>
+          <span className="mx-10 text-white">{key}</span>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent position="popper">
+              <FixedSizeList
+                height={200}
+                itemCount={itemCount}
+                itemKey={getItemKey}
+                itemSize={60}
+                width={200}
+              >
+                {renderItem}
+              </FixedSizeList>
+            </SelectContent>
+          </Select>
         </div>
       ))}
     </div>
