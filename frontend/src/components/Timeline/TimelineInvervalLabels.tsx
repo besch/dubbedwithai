3662d3c920fe18af3662380f9da001b0edbb9df5
vinog <vinogradskiy.n@gmail.com>
2024-06-13 16:26:56 +0200

    TimelineInvervalLabels update

--- frontend/src/components/Timeline/TimelineInvervalLabels.tsx
+++ frontend/src/components/Timeline/TimelineInvervalLabels.tsx
@@ -9,16 +9,19 @@ const TimelineInvervalLabels: React.FC<TimelineInvervalLabelsProps> = ({
   totalDuration,
   zoom,
 }) => {
+  const baseInterval = zoom <= 2 ? 300 * 1000 : 30 * 1000;
+  const numLabels = Math.ceil(totalDuration / baseInterval);
+
   return (
     <>
-      {Array.from({ length: zoom * 11 }, (_, i) => (
+      {Array.from({ length: numLabels }, (_, i) => (
         <div
           key={i}
           className="absolute top-0 bottom-0 border-l border-dashed border-gray-400"
-          style={{ left: `${(i * 100) / (11 * zoom)}%` }}
+          style={{ left: `${(i * 100) / numLabels}%` }}
         >
           <div className="absolute bottom-0 left-1/2 -translate-x-1/2 text-xs">
-            {formatTime((totalDuration * i) / (11 * zoom))}
+            {formatTime(i * baseInterval)}
           </div>
         </div>
       ))}
