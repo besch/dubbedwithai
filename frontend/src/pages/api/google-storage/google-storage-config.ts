a8b5abbd18d516416cb1eb282eff63ea826b8d09
vinog <vinogradskiy.n@gmail.com>
2024-07-12 14:36:59 +0200

    logs

--- frontend/src/pages/api/google-storage/google-storage-config.ts
+++ frontend/src/pages/api/google-storage/google-storage-config.ts
@@ -1,5 +1,11 @@
 import { Storage } from "@google-cloud/storage";
 
+console.log(
+  process.env.GOOGLE_CLOUD_PROJECT_ID,
+  process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+  process.env.GOOGLE_CLOUD_PRIVATE_KEY?.replace(/\\n/g, "\n")
+);
+
 const storageConfig = new Storage({
   projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
   credentials: {
