a1b691acd7adca11949458223450b0ac9cf3599c
vinog <vinogradskiy.n@gmail.com>
2024-10-14 20:23:34 +0200

    style updates

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,12 @@
-// components/Layout.tsx
 import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gradient-to-b from-gray-100 to-gray-200">
       <Navbar />
-      <main className="container mx-auto px-4 py-8">{children}</main>
+      <main className="container mx-auto px-4 py-8 bg-accent shadow-md">
+        {children}
+      </main>
     </div>
   );
 }
