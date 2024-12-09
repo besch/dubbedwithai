1b7fba6c4bdc85375f8f72c6364eef742ca296cb
vinog <vinogradskiy.n@gmail.com>
2024-12-09 21:10:03 +0100

    more cleanups

--- src/types.ts
+++ src/types.ts
@@ -5,24 +5,3 @@ export type DubbingVoice =
   | "onyx"
   | "nova"
   | "shimmer";
-
-export interface AdjustedTiming {
-  start: number;
-  end: number;
-}
-
-export interface Language {
-  id: string;
-  attributes: {
-    language: string;
-    language_name: string;
-    ratings: number;
-    download_count: number;
-    subtitle_id: string;
-    files: Array<{
-      file_id: string;
-      format: string;
-      download_count: number;
-    }>;
-  };
-}
