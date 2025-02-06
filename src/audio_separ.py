206cd4b724e69cb2bf672e8aaa69950325982e87
kolka <vinogradskiy.n@gmail.com>
2025-02-06 19:33:07 +0100

    remove old code

--- src/audio_separ.py
+++
@@ -1,9 +0,0 @@
-from audio_separator.separator import Separator
-
-separator = Separator()
-separator.load_model(model_filename='UVR-MDX-NET-Inst_HQ_3.onnx')
-
-output_file_paths = separator.separate(r"C:\Users\user\Desktop\Projects\dubbedwithai\src\source\audio - Copy.wav")
-
-# The separated vocals will be saved as the first output file
-vocals_file = output_file_paths[1]
